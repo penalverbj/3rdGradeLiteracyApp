375a6528f3ead4682fcb44840f25a05ff1119b2a
penalverbj <penalverbj@gmail.com>
2021-02-01 19:15:11 -0500

    code clean

--- WordPhonics/src/components/MainMenuScreen.js
+++ WordPhonics/src/components/MainMenuScreen.js
@@ -23,46 +23,101 @@ export default function MainMenuScreen(props, {navigation}) {
         <Text style={styles.title}>Figures of Speech</Text>
         <Text style={styles.subtitle}>fun ways to use language</Text>
         <ScrollView style={styles.scrollView}>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#fcf351'}]}>
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#fcf351'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>12: synonyms</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f3b2c8'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#f3b2c8'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>13: antonyms</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#bfe54e'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#bfe54e'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>14: homophones</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#e0696b'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#e0696b'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>15: puns</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '##cda1d2'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '##cda1d2'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>16: homographs</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f3b88c'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#f3b88c'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>17: similies & metaphors</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#c3c3c3'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#c3c3c3'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>18: onomatopeia</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#fdf885'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#fdf885'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>19: irony or sarcasm</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#a7d8e8'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#a7d8e8'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>20: personification</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f6cb47'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#f6cb47'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>21: hyperbole</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#ddc0e1'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#ddc0e1'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>22: euphemism</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#b87cbe'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#b87cbe'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>23: oxymoron</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#66cf69'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#66cf69'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>24: rhyme</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#ee7af8'}]}>
+
+          <TouchableOpacity
+            style={[styles.lesson, {backgroundColor: '#ee7af8'}]}
+            onPress={() => null}
+          >
             <Text style={styles.section}>25: alliteration</Text>
           </TouchableOpacity>
         </ScrollView>
