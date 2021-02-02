9ad6efb8eab0bf818a00cf3a9c58b304b66aef54
penalverbj <penalverbj@gmail.com>
2021-02-01 19:07:02 -0500

    added unit buttons

--- WordPhonics/src/components/MainMenuScreen.js
+++ WordPhonics/src/components/MainMenuScreen.js
@@ -11,17 +11,62 @@ import {
   View,
   Text,
   StyleSheet,
-  FlatList,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity
 } from 'react-native';
 
 export default function MainMenuScreen(props, {navigation}) {
   return (
     <>
-      <View style={styles.startContainer}>
-        <Text style={styles.section}>Lessons</Text>
-        <FlatList>
-        </FlatList>
-      </View>
+      <SafeAreaView style={styles.startContainer}>
+        <Text style={styles.title}>Figures of Speech</Text>
+        <Text style={styles.subtitle}>fun ways to use language</Text>
+        <ScrollView style={styles.scrollView}>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#fcf351'}]}>
+            <Text style={styles.section}>12: synonyms</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f3b2c8'}]}>
+            <Text style={styles.section}>13: antonyms</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#bfe54e'}]}>
+            <Text style={styles.section}>14: homophones</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#e0696b'}]}>
+            <Text style={styles.section}>15: puns</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '##cda1d2'}]}>
+            <Text style={styles.section}>16: homographs</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f3b88c'}]}>
+            <Text style={styles.section}>17: similies & metaphors</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#c3c3c3'}]}>
+            <Text style={styles.section}>18: onomatopeia</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#fdf885'}]}>
+            <Text style={styles.section}>19: irony or sarcasm</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#a7d8e8'}]}>
+            <Text style={styles.section}>20: personification</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#f6cb47'}]}>
+            <Text style={styles.section}>21: hyperbole</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#ddc0e1'}]}>
+            <Text style={styles.section}>22: euphemism</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#b87cbe'}]}>
+            <Text style={styles.section}>23: oxymoron</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#66cf69'}]}>
+            <Text style={styles.section}>24: rhyme</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.lesson, {backgroundColor: '#ee7af8'}]}>
+            <Text style={styles.section}>25: alliteration</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 }
@@ -36,13 +81,35 @@ MainMenuScreen.propTypes = {
 
 const styles = StyleSheet.create({
   startContainer: {
-    alignItems: 'flex-start',
-    flex: 4,
+    alignItems: 'center',
+    flex: 1,
     backgroundColor: '#FFFAF0',
   },
-  section: {
-    fontSize: 30,
-    margin: 10,
-    fontWeight: 'bold',
+  scrollView: {
+    //alignItems: 'center',
   },
+  title: {
+    fontSize: 40,
+    margin: 5,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  subtitle: {
+    fontSize: 20,
+    margin: 5,
+    fontWeight: '800',
+    fontStyle: 'italic',
+  },
+  lesson: {
+    borderWidth: 1,
+    //backgroundColor: 'yellow',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10
+  },
+  section: {
+    fontSize: 25,
+    margin: 5,
+    fontStyle: 'italic',
+  }
 });
