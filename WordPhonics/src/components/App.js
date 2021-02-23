a00db863adca8b15717460bf5f693abfde7f5cee
penalverbj <penalverbj@gmail.com>
2021-02-22 20:44:46 -0500

    22 finished

--- WordPhonics/src/components/App.js
+++ WordPhonics/src/components/App.js
@@ -31,6 +31,7 @@ import L18 from './L18';
 import L19 from './L19';
 import L20 from './L20';
 import L21 from './L21';
+import L22 from './L22';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -329,6 +330,22 @@ export default function App({navigation}) {
               <Stack.Screen
                 name="L21"
                 component={L21}
+                options={{
+                  headerTitle: props => <NavigationBar {...props} />, //Logo
+                  headerStyle: {backgroundColor: '#a7d8e8'},
+                  headerRight: () => ( //log out button
+                    <TouchableOpacity onPress={() => authContext.signOut()}>
+                      <Image
+                        source={require('../assets/log-out.png')}
+                        style={styles.image}
+                      />
+                    </TouchableOpacity>
+                  ),
+                }}
+              />
+              <Stack.Screen
+                name="L22"
+                component={L22}
                 options={{
                   headerTitle: props => <NavigationBar {...props} />, //Logo
                   headerStyle: {backgroundColor: '#a7d8e8'},
---
+++ WordPhonics/src/components/L22.js
@@ -0,0 +1,346 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import Sound from 'react-native-sound';
+
+export default function L22({navigation}) {
+  //counter for the example we are on. 1-13
+  const [example, setExample] = useState(1);
+  //images for rendering
+  const imgs =
+    [
+      require('../assets/22/1.png'),
+      require('../assets/22/2.png'),
+      require('../assets/22/3.png'),
+      require('../assets/22/4.png'),
+      require('../assets/22/5.png'),
+      require('../assets/22/6.png'),
+      require('../assets/22/7.png'),
+      require('../assets/22/8.png'),
+      require('../assets/22/9.png'),
+      require('../assets/22/10.png'),
+    ];
+  //list of words that come first
+  const words1 =
+    [
+      "The puppy had an accident on the rug.",
+      "He doesn't make friends easily.",
+      "She had an unusual hair style.",
+      "The boys had a disagreement.",
+      'She made a daring fashion choice.',
+      'He was displeased.',
+      "He doesn't use his study time wisely.",
+      'He seems to forget the rules about snacks.',
+      "He's not an expert driver.",
+      "Perhaps her best days as an acrobat are behind her.",
+    ];
+  //sound variables
+  var Sound = require('react-native-sound');
+  //Sound.setCategory('Playback');
+  var sound0,sound1,sound2,sound3,sound4,sound5,sound6,sound7,sound8,
+      sound9, sound10;
+  //plays definition of synonym
+  const playDef = () => {
+    sound0 = new Sound(require("../assets/22/22intro.mp3"),
+    (error, sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound0.play(() => {
+          //sound0.release();
+        });
+      });
+  }
+  //plays the audio associated with each example
+  const playEx = (num) => {
+    stopSounds();
+    switch (num) {
+      case 1:
+        sound1 = new Sound(require("../assets/22/1.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound1.play(() => {
+              sound1.release();
+            });
+          });
+        break;
+      case 2:
+        sound2 = new Sound(require("../assets/22/2.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound2.play(() => {
+              sound2.release();
+            });
+          });
+        break;
+      case 3:
+        sound3 = new Sound(require("../assets/22/3.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound3.play(() => {
+              sound3.release();
+            });
+          });
+        break;
+      case 4:
+        sound4 = new Sound(require("../assets/22/4.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound4.play(() => {
+              sound4.release();
+            });
+          });
+        break;
+      case 5:
+        sound5 = new Sound(require("../assets/22/5.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound5.play(() => {
+              sound5.release();
+            });
+          });
+        break;
+      case 6:
+        sound6 = new Sound(require("../assets/22/6.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound6.play(() => {
+             sound6.release();
+            });
+          });
+        break;
+      case 7:
+        sound7 = new Sound(require("../assets/22/7.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound7.play(() => {
+              sound7.release();
+            });
+          });
+        break;
+      case 8:
+        sound8 = new Sound(require("../assets/22/8.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound8.play(() => {
+              sound8.release();
+            });
+          });
+        break;
+      case 9:
+        sound9 = new Sound(require("../assets/22/9.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound9.play(() => {
+              sound9.release();
+            });
+          });
+        break;
+      case 10:
+        sound10 = new Sound(require("../assets/22/10.mp3"),
+        (error, sound) => {
+            if (error) {
+              alert('error' + error.message);
+              return;
+            }
+            sound10.play(() => {
+              sound10.release();
+            });
+          });
+        break;
+  }
+}
+  //stops any sound playing
+  const stopSounds = () => {
+    if (sound0) {sound0.stop();}
+    if (sound1) {sound1.stop();}
+    if (sound2) {sound2.stop();}
+    if (sound3) {sound3.stop();}
+    if (sound4) {sound4.stop();}
+    if (sound5) {sound5.stop();}
+    if (sound6) {sound6.stop();}
+    if (sound7) {sound7.stop();}
+    if (sound8) {sound8.stop();}
+    if (sound9) {sound9.stop();}
+    if (sound10) {sound10.stop();}
+  }
+    return (
+      <>
+        <View style={styles.startContainer}>
+          <TouchableOpacity onPress={() => {
+            stopSounds();
+            playDef();
+          }}>
+            <Text style={styles.subtitle}>
+              Euphemism is finding an extremely polite and nice way
+              to speak about something that is not very nice.
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.subContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              setExample(example === 1 ? imgs.length : example - 1);
+              playEx(example=== 1 ? imgs.length : example - 1);
+              stopSounds();
+            }}
+            style={styles.arrowContainer}
+          >
+            <Image
+              source={require('../assets/arrow-left.png')}
+              style={styles.arrow}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              stopSounds();
+              playEx(example);
+            }}
+          >
+            <Image
+              source={imgs[example - 1]}
+              style={styles.picture}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setExample(example === imgs.length ? 1 : example + 1);
+              playEx(example === imgs.length ? 1 : example + 1);
+              stopSounds();
+            }}
+            style={styles.arrowContainer}
+          >
+            <Image
+              source={require('../assets/arrow-right.png')}
+              style={styles.arrow}
+            />
+          </TouchableOpacity>
+          <Text style={styles.words}>{words1[example - 1]}</Text>
+        </View>
+        <View style={styles.quizContainer}>
+          <TouchableOpacity onPress={null} style={styles.quizButton}>
+            <Text style={styles.quiz}>?</Text>
+          </TouchableOpacity>
+        </View>
+        </>
+    );
+}
+
+L22.navigationOptions = () => {(
+    title:'L22'
+)}
+
+const styles = StyleSheet.create({
+  startContainer: {
+    justifyContent: 'center',
+    flex: 3,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    backgroundColor: '#FFFAF0',
+    paddingTop: 2,
+    paddingBottom: 14,
+  },
+  subContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    flex: 10,
+    flexDirection: 'row',
+    backgroundColor: '#FFFAF0',
+    paddingTop: 10,
+    flexWrap: 'wrap',
+  },
+  subtitle: {
+    fontSize: 20,
+    margin: 5,
+    fontWeight: '800',
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  arrowContainer: {
+    backgroundColor: 'green',
+    borderWidth: 1,
+    borderRadius: 35,
+    padding: 5,
+    marginTop: 130,
+  },
+  arrow: {
+    height: 35,
+    width: 35,
+  },
+  picture: {
+    height: 250,
+    width: 250,
+    resizeMode: 'stretch',
+  },
+  words: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginHorizontal: 20,
+    textAlign: 'center',
+  },
+  quizContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFAF0',
+    paddingBottom: 70,
+    flexDirection: 'row',
+  },
+  quizButton: {
+    backgroundColor: '#bfe54e',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 30,
+    width: 50,
+    height: 50,
+  },
+  quiz: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
