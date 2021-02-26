7d2a58541443884a0b4179f5c8236b62ba1be70a
nathandaven <nathandaven@gmail.com>
2021-02-26 18:01:09 -0500

    Working answer randomizer implemented

--- WordPhonics/src/components/Q12M1.js
+++ WordPhonics/src/components/Q12M1.js
@@ -19,6 +19,8 @@ import PropTypes from 'prop-types';
 import Sound from 'react-native-sound';
 
 export default function Q12M1({navigation}) {
+
+
   // list of correct answers
   const correctPairs =
     [
@@ -54,16 +56,90 @@ export default function Q12M1({navigation}) {
 
       ];
 
+    //const currentPairs = [];
+    const [answerPair, setAnswerPair] = useState(1);
+
+
     // question to be asked at top -- maybe we could generalize this
     // quiz screen
     const question = "Which two words are synonyms with similar meanings?";
 
+    // runs when you click the question for testing
+    const generateQuestion = () => {
+      // fills with random incorect
+      let currentPairs = [
+          incorrectPairs[pickRandom(0, incorrectPairs.length)],
+          incorrectPairs[pickRandom(1, incorrectPairs.length)],
+          incorrectPairs[pickRandom(2, incorrectPairs.length)],
+          incorrectPairs[pickRandom(3, incorrectPairs.length)]
+      ];
+
+      // randomizes the location of random pair and picks a random answer
+      currentPairs[pickRandom(0,4)] = correctPairs[pickRandom(0, correctPairs.length)];
+
+      setAnswerPair(currentPairs);
+    }
+
+    const checkAnswer = (index) => {
+
+    }
+
+    const pickRandom =  (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+        //The maximum is exclusive and the minimum is inclusive
+    }
+
+
+
   return (
+    <>
     <View style={styles.startContainer}>
-        <Text style={styles.subtitle}>
-          {question}
-        </Text>
+        <TouchableOpacity onPress = {() => {generateQuestion();}}>
+            <Text style={styles.subtitle}>
+              {question}
+            </Text>
+        </TouchableOpacity>
     </View>
+
+
+    <View style={styles.subContainer}>
+        <TouchableOpacity onPress = {() => {
+            checkAnswer(0);
+          }}>
+            <Text style={styles.answer}>
+              {answerPair[0]}
+            </Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity onPress = {() => {
+            checkAnswer(1);
+          }}>
+            <Text style={styles.answer}>
+              {answerPair[1]}
+            </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress = {() => {
+            checkAnswer(2);
+          }}>
+            <Text style={styles.answer}>
+              {answerPair[2]}
+            </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress = {() => {
+            checkAnswer(3);
+          }}>
+            <Text style={styles.answer}>
+              {answerPair[3]}
+            </Text>
+        </TouchableOpacity>
+    </View>
+    </>
+
     );
 }
 
@@ -102,10 +178,14 @@ const styles = StyleSheet.create({
     marginHorizontal: 50,
     textAlign: 'center',
   },
-  answerContainer: {
-    height: 250,
-    width: 250,
-    resizeMode: 'stretch',
+  answer: {
+    fontSize: 40,
+    margin: 20,
+    backgroundColor: "grey",
+    color: "white",
+    fontWeight: '800',
+    fontStyle: 'italic',
+    textAlign: 'center',
   },
 
 });
